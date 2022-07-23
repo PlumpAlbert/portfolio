1a3d63f55794780934ba643b28b45dfdd33fc7cd
plump_albert <plumpalbert@gmail.com>
2022-07-23 18:15:50 +0300

    created static markup for statistic view from design

---
+++ src/views/StatsView/StatLine.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from "vue"
+
+const props = defineProps({
+  label: String,
+  value: String,
+});
+
+let color = ref(`var(--${props.label.toLowerCase().replace(' ', '-')}-color)`);
+</script>
+
+<template>
+  <p class="flex flex-row gap-2 items-baseline">
+    <i class="w-2 h-2 rounded-full" :style="'background-color:' + color"></i>
+    <span class="text-base font-medium"> {{ props.label }} </span>
+    <hr class="flex-1 border-dashed border-neutral-100 opacity-40" />
+    <span class="text-base font-sans font-normal">{{ props.value }}</span>
+  </p>
+</template>
---
+++ src/views/StatsView/index.vue
@@ -0,0 +1,41 @@
+<script setup>
+import {ref} from "vue";
+import StatLine from "./StatLine.vue";
+
+const date = ref(new Date());
+const stats = [
+  {label: "Very Productive", value: "3h 10m"},
+  {label: "Productive", value: "13m 45s"},
+  {label: "Neutral", value: "3m 18s"},
+  {label: "Distracting", value: "0s"},
+  {label: "Very Distracting", value: "5m 2s"},
+];
+</script>
+
+<template>
+  <main class="flex flex-col gap-16 pb-12">
+    <div class="flex flex-row justify-between align-center">
+      <button
+        class="flex items-center justify-center p-2 rounded-full text-neutral-400 hover:bg-neutral-100/10"
+      >
+        <i class="material-symbols-rounded">chevron_left</i>
+      </button>
+      <h3
+        class="text-xl font-semibold font-sans text-uppercase flex items-center"
+      >
+        {{ date.toLocaleDateString() }}
+      </h3>
+      <button
+        class="flex items-center justify-center p-2 rounded-full text-neutral-400 hover:bg-neutral-100/10"
+      >
+        <i class="material-symbols-rounded">chevron_right</i>
+      </button>
+    </div>
+
+    <div class="chart self-center w-[250px] h-[250px] bg-teal-900"></div>
+
+    <section class="flex flex-col gap-5">
+      <StatLine v-for="stat in stats" :label="stat.label" :value="stat.value" />
+    </section>
+  </main>
+</template>
