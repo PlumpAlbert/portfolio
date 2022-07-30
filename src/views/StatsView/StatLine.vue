15152a166d357ed45ad8a49db9b803c7fa8c90b3
plump_albert <plumpalbert@gmail.com>
2022-07-30 12:36:37 +0300

    updated StatLine component to display data from RescueTime response

--- src/views/StatsView/StatLine.vue
+++ src/views/StatsView/StatLine.vue
@@ -1,19 +1,39 @@
 <script setup>
 import { ref } from "vue"
+import intervalToDuration from "date-fns/intervalToDuration";
+
+const LABELS = {
+  2: 'Very productive',
+  1: 'Productive',
+  0: 'Neutral',
+  '-1': 'Distracting',
+  '-2': 'Very distracting',
+}
 
 const props = defineProps({
   label: String,
   value: String,
 });
 
-let color = ref(`var(--${props.label.toLowerCase().replace(' ', '-')}-color)`);
+const label = LABELS[props.label];
+
+let color = ref(`var(--${label.toLowerCase().replace(' ', '-')}-color)`);
+
+const duration = intervalToDuration({start: 0, end: props.value * 1000});
+
+let time = '';
+if (duration.seconds > 0) time = `${duration.seconds} s`;
+if (duration.minutes > 0) time = `${duration.minutes} min ${time}`;
+if (duration.hours > 0) time = `${duration.hours} h ${time}`;
 </script>
 
 <template>
   <p class="flex flex-row gap-2 items-baseline">
     <i class="w-2 h-2 rounded-full" :style="'background-color:' + color"></i>
-    <span class="text-base font-medium"> {{ props.label }} </span>
+    <span class="text-base font-medium"> {{ label }} </span>
     <hr class="flex-1 border-dashed border-neutral-100 opacity-40" />
-    <span class="text-base font-sans font-normal">{{ props.value }}</span>
+    <span class="text-base font-sans font-normal">
+      {{ time }}
+    </span>
   </p>
 </template>
