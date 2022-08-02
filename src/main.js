bf7dd296d06aa17b461d8dbde5cefc657a8fe1dc
plump_albert <plumpalbert@gmail.com>
2022-08-02 13:13:43 +0300

    added persistent plugin for pinia

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import "flowbite";
 import {createApp} from "vue";
 import {createPinia} from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import router from "./router";
 
 import App from "./App.vue";
@@ -8,8 +9,10 @@ import App from "./App.vue";
 import "./assets/main.css";
 
 const app = createApp(App);
+const store = createPinia();
+store.use(piniaPluginPersistedstate);
 
-app.use(createPinia());
+app.use(store);
 app.use(router);
 
 app.mount("#app");
