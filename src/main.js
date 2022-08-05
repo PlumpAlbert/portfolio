5f6d6eaeedb920b3e44197981657eb5b341b2206
plump_albert <plumpalbert@gmail.com>
2022-08-05 23:38:28 +0300

    using overmind with Vue app

--- src/main.js
+++ src/main.js
@@ -3,12 +3,18 @@ import {createApp} from "vue";
 import {createPinia} from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import router from "./router";
+// overmind
+import {createOvermind} from "overmind";
+import {withOvermind} from "overmind-vue/vue3";
+import config from "./model";
 
 import App from "./App.vue";
 
 import "./assets/main.css";
 
-const app = createApp(App);
+const app = createApp(
+  withOvermind(createOvermind(config, {devtools: true}), App)
+);
 const store = createPinia();
 store.use(piniaPluginPersistedstate);
 
