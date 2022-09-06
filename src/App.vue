d2b0a5c93bda34187eaa682879bf0eb44aa30d07
plump_albert <plumpalbert@gmail.com>
2022-09-06 23:39:11 +0300

    fix: using default value for router navigation

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,12 @@ import {routes} from "./router";
   <RouterView class="px-5 py-10" />
 
   <footer>
-    <nav class="flex flex-row gap-4 text-sm ">
+    <nav class="flex flex-row gap-4 text-sm">
       <RouterLink
         v-for="route in routes"
+        :key="route.path"
         class="px-3 py-2 flex-1 flex flex-col gap-1 items-center rounded hover:bg-neutral-100/10"
-        :to="route.path"
+        :to="route.default || route.path"
       >
         <i class="material-symbols-rounded">{{ route.icon }}</i>
         <span class="font-sans capitalize">{{ route.label }}</span>
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import {createRouter, createWebHistory} from "vue-router";
+import formatISO from "date-fns/formatISO";
 import LoginView from "@/views/LoginView.vue";
 import DayView from "@/views/DayView/index.vue";
 
@@ -6,6 +7,7 @@ export const routes = [
   {path: "/", component: LoginView, label: "home", icon: "home"},
   {
     path: "/day/:date",
+    default: "/day/" + formatISO(new Date(), {representation: "date"}),
     component: DayView,
     label: "statistics",
     icon: "bar_chart",
