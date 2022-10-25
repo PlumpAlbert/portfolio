024b004ab42e28ed7c516e2a9afa61b7d86fc463
plump_albert <plumpalbert@gmail.com>
2022-10-25 08:48:13 +0300

    added route for fetching data for a single day

--- src/server/routes/rescueTime.ts
+++ src/server/routes/rescueTime.ts
@@ -2,6 +2,7 @@ import { trpc } from "@server/index"
 import { TRPCError } from "@trpc/server"
 import { z } from "zod"
 import axios from "axios"
+import { ProductivityRank } from "src/types"
 
 function getISODate(date: Date | string) {
   if (typeof date === "string") {
@@ -11,6 +12,51 @@ function getISODate(date: Date | string) {
 }
 
 const router = trpc.router({
+  day: trpc.procedure.input(z.date()).query(async ({ input, ctx }) => {
+    if (!ctx.session) {
+      throw new TRPCError({
+        code: "FORBIDDEN",
+        message: "You must be authorized to invoke this method",
+      })
+    }
+    try {
+      const { data } = await axios.get<{
+        notes: string
+        row_headers: string[]
+        rows: Array<[string, number, number, number]>
+      }>("https://www.rescuetime.com/anapi/data", {
+        params: {
+          key: ctx.session.id,
+          format: "json",
+          by: "interval",
+          interval: "day",
+          restrict_kind: "productivity",
+          restrict_begin: getISODate(input),
+          restrict_end: getISODate(input),
+        },
+      })
+      const result: { date: string; ranks: Record<number, number> } = {
+        date: getISODate(input),
+        ranks: {
+          [ProductivityRank.VERY_PRODUCTIVE]: 0,
+          [ProductivityRank.PRODUCTIVE]: 0,
+          [ProductivityRank.NEUTRAL]: 0,
+          [ProductivityRank.DISTRACTING]: 0,
+          [ProductivityRank.VERY_DISTRACTING]: 0,
+        },
+      }
+      if (data.rows.length !== 0) {
+        data.rows.forEach(row => {
+          result.ranks[row[AnalyticDataIndexes.PRODUCTIVITY]] =
+            row[AnalyticDataIndexes.TIME_SPENT]
+        })
+      }
+      return result
+    } catch (err) {
+      console.error("# error: ", err)
+      return
+    }
+  }),
   data: trpc.procedure
     .input(
       z.object({
