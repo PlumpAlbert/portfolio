c7c7b5899d72c99bd407bfc7226da1701fe632a2
plump_albert <plumpalbert@gmail.com>
2022-10-25 11:48:38 +0300

    added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,23 @@ module.exports = {
   content: ["src/**/*.{tsx,sass,css}"],
   theme: {
     extend: {
+      colors: {
+        background: {
+          light: "#E6E6E6",
+          dark: "#1E1E1E",
+        },
+        productivity: {
+          2: "#8EABE6",
+          1: "#87EEBB",
+          0: "#D9D9D9",
+          ["-1"]: "#E9E463",
+          ["-2"]: "#FF8766",
+        },
+      },
       fontFamily: {
         ...defaults.fontFamily,
         sans: ["Inter", ...defaults.fontFamily.sans],
+        header: ["Bebas Neue", "Inter", ...defaults.fontFamily.sans],
       },
     },
   },
