92b816eaf88c8969ce0afe0c42995fe7c320a0ff
plump_albert <plumpalbert@gmail.com>
2022-11-19 13:31:12 +0300

    added productivity colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
   content: ["src/**/*.{tsx,sass,css}"],
   theme: {
     extend: {
+      colors: {
+        productivity: {
+          2: "#8EABE6",
+          1: "#87EEBB",
+          0: "#D9D9D9",
+          ['-1']: "#E9E463",
+          ['-2']: "#FF8766",
+        },
+      },
       fontFamily: {
         ...defaults.fontFamily,
         sans: ["Inter", ...defaults.fontFamily.sans],
