3a29eefc835f3a2fe850a90c0d5f1af851e3f073
plump_albert <plumpalbert@gmail.com>
2023-01-27 14:42:39 +0300

    injecting custom css-class to body

--- src/pages/postcard/valentine/2023/index.tsx
+++ src/pages/postcard/valentine/2023/index.tsx
@@ -1,19 +1,29 @@
-import type { NextPage } from "next"
-import Image from "next/image"
-import Head from "next/head"
+import {useEffect} from "react";
+import type {NextPage} from "next";
+import Image from "next/image";
+import Head from "next/head";
+import styles from "./styles.module.css";
 
 const Page: NextPage = () => {
+  useEffect(() => {
+    document.body.classList.add(styles.body);
+    return () => document.body.classList.remove(styles.body);
+  });
   return (
-    <main className="px-4 py-8 flex-1 flex flex-col gap-6 items-center">
+    <main className="m-auto px-4 py-8 flex-1 flex flex-col gap-6 items-center">
       <Head>
         <title>Happy Valentine&apos;s Day</title>
       </Head>
-      <h1 className="m-0 text-2xl text-blue-400 self-center uppercase">
-        Happy Valentine&apos;s Day
+      <h1
+        className="m-0 text-center text-4xl text-[#F6C1C1] self-center uppercase"
+        style={{fontFamily: "Kaushan Script"}}
+      >
+        Happy
+        <br /> Valentine&apos;s Day
       </h1>
 
-      <div className="w-full h-full flex flex-col gap-4">
-        <div className="self-center w-1/2">
+      <div className="w-full h-full flex flex-col gap-5">
+        <div className="self-center w-3/4">
           <Image
             className="rounded-md"
             alt="valentine.png"
@@ -22,16 +32,13 @@ const Page: NextPage = () => {
             height={999999}
             quality="100"
           />
-
-          <audio controls className="w-full">
-            <source
-              src="https://docs.google.com/uc?authuser=0&export=download&id=1diJp41iGukFb2zdF8u7RB6OJ5meIJwU5"
-            />
-          </audio>
         </div>
+        <audio controls className="w-full">
+          <source src="https://docs.google.com/uc?authuser=0&export=download&id=1P_6rhoM0AYxPfytNBAV9IdowbQIdyZ_u" />
+        </audio>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
