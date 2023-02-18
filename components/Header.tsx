9340e53eda95e7f80ea84bb0ca067442534955c5
plump_albert <plumpalbert@gmail.com>
2023-02-18 13:20:33 +0300

    updated `Header` component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import styles from "./Header.module.scss"
 import { sassBuilder } from "@/utils/sass"
 
@@ -5,9 +6,15 @@ const x = sassBuilder(styles)
 
 const Header = () => (
 	<header className={x({ header: true })}>
-		<i className={x({icon: true, logo: true}, "msr")}>person</i>
+		<Image
+			className={x({ icon: true, logo: true })}
+			src="/deer.png"
+			height={32}
+			width={32}
+			alt="deer"
+		/>
 		<h1 className={x({ "header-title": true })}>Plump Albert</h1>
-		<i className={x({icon: true, menu: true}, "msr")}>menu</i>
+		<i className={x({ icon: true, menu: true }, "msr")}>menu</i>
 	</header>
 )
 
