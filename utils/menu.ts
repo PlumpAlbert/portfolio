7d17080891c132fd106e1cc368c404a9614e965a
plump_albert <plumpalbert@gmail.com>
2023-02-28 17:38:36 +0300

    added menu navigation URLs

---
+++ utils/menu.ts
@@ -0,0 +1,11 @@
+export type MenuItem = {
+	label: string
+	href: string
+}
+
+const menuItems: MenuItem[] = [
+	{ label: "Postcards", href: "/postcards" },
+	{ label: "Statistics", href: "/analytics" },
+]
+
+export default menuItems
