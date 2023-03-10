9dcfb3b173ee7c767978d0e6a9bde4ae182a8104
plump_albert <plumpalbert@gmail.com>
2023-03-10 12:03:45 +0300

    added ability to switch current month

--- pages/stats/rescuetime.tsx
+++ pages/stats/rescuetime.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react"
-import { useQuery } from "react-query"
+import { MouseEventHandler, useCallback, useState } from "react"
+import { useQuery, useQueryClient } from "react-query"
 // next
 import axios from "axios"
 import type { NextPage } from "next"
 import Head from "next/head"
 // date-fns
-import intervalToDuration from "date-fns/intervalToDuration"
+import { intervalToDuration, format, addMonths, isSameMonth } from "date-fns"
 // imports
 import { Productivity } from "@/pages/api/stats/rescuetime/month"
 // styles
@@ -23,15 +23,30 @@ function getDuration(time?: number) {
 
 const RescueTimePage: NextPage = () => {
 	const [date, setDate] = useState(new Date())
+	const client = useQueryClient()
 
-	const { isLoading, data } = useQuery("analytics", async function () {
-		const { data } = await axios.get<{
-			data: Record<string, Productivity>
-		}>("/api/stats/rescuetime/month", {
-			params: { date: date.toISOString() },
-		})
-		return data.data
-	})
+	const { isLoading, data } = useQuery(
+		["analytics", date] as const,
+		async function ({ queryKey }) {
+			const [, date] = queryKey
+			const { data } = await axios.get<{
+				data: Record<string, Productivity>
+			}>("/api/stats/rescuetime/month", {
+				params: { date: date.toISOString() },
+			})
+			return data.data
+		}
+	)
+
+	const handleForwardClick = useCallback<MouseEventHandler>(e => {
+		e.stopPropagation()
+		setDate(old => addMonths(old, 1))
+	}, [])
+
+	const handleBackClick = useCallback<MouseEventHandler>(e => {
+		e.stopPropagation()
+		setDate(old => addMonths(old, -1))
+	}, [])
 
 	/**
 	 * @param date
@@ -40,26 +55,26 @@ const RescueTimePage: NextPage = () => {
 	const printRow = (date: string, productivity: Productivity) => {
 		const d = new Date(date)
 		return (
-			<div key={date} className={x({ row: true })}>
-				<p className={x({ cell: true, date: true })}>
+			<tr key={date} className={x({ row: true })}>
+				<td className={x({ cell: true, date: true })}>
 					{d.toLocaleDateString()}
-				</p>
-				<p className={x({ cell: true, "very-productive": true })}>
+				</td>
+				<td className={x({ cell: true, "very-productive": true })}>
 					{getDuration(productivity.veryProductive)}
-				</p>
-				<p className={x({ cell: true, productive: true })}>
+				</td>
+				<td className={x({ cell: true, productive: true })}>
 					{getDuration(productivity.productive)}
-				</p>
-				<p className={x({ cell: true, neutral: true })}>
+				</td>
+				<td className={x({ cell: true, neutral: true })}>
 					{getDuration(productivity.neutral)}
-				</p>
-				<p className={x({ cell: true, distracting: true })}>
+				</td>
+				<td className={x({ cell: true, distracting: true })}>
 					{getDuration(productivity.distracting)}
-				</p>
-				<p className={x({ cell: true, "very-distracting": true })}>
+				</td>
+				<td className={x({ cell: true, "very-distracting": true })}>
 					{getDuration(productivity.veryDistracting)}
-				</p>
-			</div>
+				</td>
+			</tr>
 		)
 	}
 
@@ -69,79 +84,97 @@ const RescueTimePage: NextPage = () => {
 				<title>RescueTime stats</title>
 			</Head>
 			<main className={x({ root: true })}>
+				<header className={x({ header: true })}>
+					<button
+						className={x({ "icon-button": true }, "icon-button")}
+						onClick={handleBackClick}
+					>
+						<i className="msr">arrow_back_ios</i>
+					</button>
+					<h2 className={x({ header__title: true })}>
+						{format(date, "MMMM, yyyy")}
+					</h2>
+					<button
+						disabled={isSameMonth(new Date(), date)}
+						className={x({ "icon-button": true }, "icon-button")}
+						onClick={handleForwardClick}
+					>
+						<i className="msr">arrow_forward_ios</i>
+					</button>
+				</header>
 				{isLoading && (
 					<div className={x({ "spinner-wrapper": true })}>
 						<i className={x({ spinner: true }, "msr")}>sync</i>
 					</div>
 				)}
-				<div className={x({ table: true })}>
-					<div className={x({ head: true })}>
-						<div className={x({ row: true })}>
-							<p
+				<table className={x({ table: true })}>
+					<thead className={x({ head: true })}>
+						<tr className={x({ row: true })}>
+							<th
 								className={x({
 									cell: true,
-									header: true,
+									head: true,
 									date: true,
 								})}
 							>
 								<i className="msr">calendar_month</i>
-							</p>
-							<p
+							</th>
+							<th
 								className={x({
 									cell: true,
-									header: true,
+									head: true,
 									"very-productive": true,
 								})}
 							>
 								<i className="msr">keyboard_double_arrow_up</i>
-							</p>
-							<p
+							</th>
+							<th
 								className={x({
 									cell: true,
-									header: true,
+									head: true,
 									productive: true,
 								})}
 							>
 								<i className="msr">expand_less</i>
-							</p>
-							<p
+							</th>
+							<th
 								className={x({
 									cell: true,
-									header: true,
+									head: true,
 									neutral: true,
 								})}
 							>
 								<i className="msr">remove</i>
-							</p>
-							<p
+							</th>
+							<th
 								className={x({
 									cell: true,
-									header: true,
+									head: true,
 									distracting: true,
 								})}
 							>
 								<i className="msr">expand_more</i>
-							</p>
-							<p
+							</th>
+							<th
 								className={x({
 									cell: true,
-									header: true,
+									head: true,
 									"very-distracting": true,
 								})}
 							>
 								<i className="msr">
 									keyboard_double_arrow_down
 								</i>
-							</p>
-						</div>
-					</div>
-					<div className={x({ body: true })}>
+							</th>
+						</tr>
+					</thead>
+					<tbody className={x({ body: true })}>
 						{data &&
 							Object.keys(data).map(key =>
 								printRow(key, data[key])
 							)}
-					</div>
-				</div>
+					</tbody>
+				</table>
 			</main>
 		</>
 	)
